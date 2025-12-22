0a5297e3d44df87b363263b188f71a662d5d9e7e
Wesley Ng <wesleyng2003@gmail.com>
2025-12-22 13:57:43 -0800

    Added zip compiler

---
+++ zip.ps1
@@ -0,0 +1,16 @@
+$folder = "$PSScriptRoot\bin"
+
+if (-not (Test-Path -path $folder)) {
+    New-Item -Path $folder -ItemType Directory
+}
+
+Remove-Item -Path "$folder\*.*"
+
+$accepted = @(".asl")
+$files = Get-ChildItem -Path $PSScriptRoot | where {$_.extension -in $accepted}
+
+foreach ($file in $files) {
+    Compress-Archive -LiteralPath "$PSScriptRoot\$($file.Name)" -DestinationPath "$folder\$($file.Name)".replace(".asl", ".zip")
+}
+
+"[$(Get-Date)]: ASL files have been zipped and placed in the bin folder."
